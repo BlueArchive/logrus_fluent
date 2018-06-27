ee798cbbbdffa8eaa6e08e4e28912b6c2b7f5bab
Andy Brown <andybrown668@gmail.com>
2018-06-27 10:14:39 +0100

    wasabi: connection seriualization and global/local tagging

--- fluent.go
+++ fluent.go
@@ -3,6 +3,8 @@ package logrus_fluent
 import (
 	"github.com/Sirupsen/logrus"
 	"github.com/fluent/fluent-logger-golang/fluent"
+	"sync"
+	"fmt"
 )
 
 const (
@@ -20,16 +22,18 @@ var defaultLevels = []logrus.Level{
 }
 
 type fluentHook struct {
+	config fluent.Config
 	host   string
 	port   int
 	levels []logrus.Level
 	tag    *string
+	mu     sync.Mutex
+	logger *fluent.Fluent
 }
 
-func NewHook(host string, port int) *fluentHook {
+func NewHook(config fluent.Config) *fluentHook {
 	return &fluentHook{
-		host:   host,
-		port:   port,
+		config: config,
 		levels: defaultLevels,
 		tag:    nil,
 	}
@@ -61,34 +65,36 @@ func setMessage(entry *logrus.Entry, data logrus.Fields) {
 }
 
 func (hook *fluentHook) Fire(entry *logrus.Entry) error {
-	logger, err := fluent.New(fluent.Config{
-		FluentHost: hook.host,
-		FluentPort: hook.port,
-	})
-	if err != nil {
-		return err
-	}
-	defer logger.Close()
-
 	// Create a map for passing to FluentD
 	data := make(logrus.Fields)
 	for k, v := range entry.Data {
-		data[k] = v
+		data[k] = fmt.Sprintf("%v", v)
 	}
 
 	setLevelString(entry, data)
 	var tag string
-	if hook.tag == nil {
-		tag = getTagAndDel(entry, data)
+		tag = *hook.tag
 		if tag != entry.Message {
 			setMessage(entry, data)
 		}
-	} else {
-		tag = *hook.tag
-	}
 
 	fluentData := ConvertToValue(data, TagName)
-	err = logger.PostWithTime(tag, entry.Time, fluentData)
+
+	// serialize control of the connection creation and sending of data to fluentd
+	hook.mu.Lock()
+	defer hook.mu.Unlock()
+	// create a connection and retain it
+	// note the connection is not closed; if there's an error using the connection closes itself
+	// and we'll notice that and reconnect on the next Fire. This reproduces this original library's implementation
+	// (ie, a message gets lost)
+	if hook.logger == nil || hook.logger.IsClosed() {
+		var err error
+		if hook.logger, err = fluent.New(hook.config); err != nil {
+			return err
+		}
+	}
+
+	err := hook.logger.PostWithTime(tag, entry.Time, fluentData)
 	return err
 }
 
